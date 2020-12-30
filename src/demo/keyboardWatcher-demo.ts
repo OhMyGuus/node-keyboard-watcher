5909b00f52056749ee591a885420b87d080e7923
OhMyGuus <info@guus.ninja>
2020-12-30 01:00:07 +0100

    Small fix2

--- src/demo/keyboardWatcher-demo.ts
+++ src/demo/keyboardWatcher-demo.ts
@@ -1,8 +1,7 @@
 import { keyboardWatcher } from '..'
 
-// overlayWindow.addListener(0x42); // key B
 keyboardWatcher.start(); // key B
 
 
-keyboardWatcher.addKeyHook()
+keyboardWatcher.addKeyHook(0x42)
 process.stdin.resume();

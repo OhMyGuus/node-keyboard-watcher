408c94b512a2f97ac6a03036a61fc1d6e6a7b0fc
OhMyGuus <info@guus.ninja>
2021-01-31 21:31:04 +0100

    Change forgotten char to int

--- src/demo/keyboardWatcher-demo.ts
+++ src/demo/keyboardWatcher-demo.ts
@@ -3,15 +3,15 @@ import { keyboardWatcher } from '..'
 keyboardWatcher.start(); // key B
 
 
-keyboardWatcher.addKeyHook(0x42)
-console.log("Ketwatcger started abd watching input for key: B");
+keyboardWatcher.addKeyHook(162)
+console.log("Started keyboard watcher.. ");
 
 keyboardWatcher.on('keydown', (keyid) => {
-    console.log("Keydown event: ", keyid);
+    console.log("Keydown event: ", keyid.toString(16).toUpperCase());
 });
 
 keyboardWatcher.on('keyup', (keyid) => {
-    console.log("Keyup event: ", keyid);
+    console.log("Keyup event: ", keyid.toString(16).toUpperCase());
 });
 
 process.stdin.resume();
